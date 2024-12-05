9dadce5ff70e6ac2cbe67d2b07371e4830d65d64
eduardaa-ferreira <ferreira21012006@gmail.com>
2024-12-05 09:48:38 -0300

    Add arquivo aula 02
    Eduarda Ferreira - Aula 02 de Python (o5/12/2024)

---
+++ Eduarda - AULA 02/Aula02.py
@@ -0,0 +1,11 @@
+print ('a meta é correr atrás dos objetivos porque eles não vão vir te atropelar')
+
+nome_usuario = 'Eduarda Ferrreira' # tipo -> str = string
+idade_usuario = 18 # tipo -> int = integrar
+altura_usuario = 1.68 # tipo -> float = ponto flutuante
+maior_idade = True # tipo-> bppç = boolean
+
+print ( 'Seja Bem-Vindo ', nome_usuario)
+print ('Sua idade é' , idade_usuario , 'anos') 
+print ('você tem ' , altura_usuario , 'metros')
+print ('Você é maior de idade ?' , maior_idade)
